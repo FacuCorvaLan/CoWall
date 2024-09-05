b7707d7dd900f2be1061c142722b229ccb2af7d4
Facundo Corvalán <facundocorvalan2009@gmail.com>
2024-09-05 08:59:42 -0300

    Create proyect

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <InitialPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import InitialPage from "../src/components/InitialPage.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InitialPage
   }
 }
 </script>
---
+++ src/components/InitialPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <div class="containerNav">
+      <div class="logoCw">
+        <img src="../assets/Images/logo-CoWall.png" class="imgLogo">
+        <h2>Cowall</h2>
+      </div>
+      <div class="BtnSesion">
+        <form>
+          <input type="button" class="btnUsers" value="Iniciar Sesión">
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <body>
+    <div class="containerPpal">
+      <div>
+        <h1>Cambia tu moneda, cambia tu futuro.</h1>
+      </div>
+      <div><img></div>
+    </div>
+  </body>
+</template>
+
+<style>
+.containerNav{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.imgLogo{
+  max-width: 55px;
+}
+</style>
